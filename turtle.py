35f3448f34f6bd9f355c807ce0ba066aaa3e83b7
Rishikesh <91647089+i-rishikesh@users.noreply.github.com>
2022-12-30 16:40:04 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,16 @@
+import turtle
+s=turtle.getscreen()
+t=turtle.Turtle()
+t.right(90)
+t.fd(100)
+t.lt(90)
+t.backward(100)
+t.goto(100,100)
+t.home()
+t.circle(90)
+t.dot(20)
+turtle.bgcolor("black")
+turtle.title("kch to nya bnayenge")
+s.end_fill()
+
+
